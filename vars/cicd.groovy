2acb9b8aec3343e27617bc2124ccfacf2d1a216d
Jayaramireddy Vadisana <61652069+vjreddy1619@users.noreply.github.com>
2023-08-03 10:52:14 +0100

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,5 +8,5 @@ def tomcatDeployment(jobname,ip,context){
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
 def runSeleinium(jobname){
-  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
