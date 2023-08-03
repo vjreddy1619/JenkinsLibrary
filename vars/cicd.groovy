630cdb0c8ea1cc2de75d4023727a5c4456d1c9a1
Jayaramireddy Vadisana <61652069+vjreddy1619@users.noreply.github.com>
2023-08-03 09:53:45 +0100

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -4,3 +4,6 @@ def gitDownload(repo){
 def mavenBuild(){
 	sh 'mvn package'
 }
+def tomcatDeployment(jobname,ip,context){
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
